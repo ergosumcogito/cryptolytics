9732b45e46127129a6dea63c9d8c2c4142bfad57
ergosumcogito <ergosumcogito@users.noreply.github.com>
2024-12-25 20:05:30 +0100

    Delete not used port

--- src/main/java/com/backendproject/cryptolytics/domain/port/in/CryptoQueryPort.java
+++
@@ -1,7 +0,0 @@
-package com.backendproject.cryptolytics.domain.port.in;
-
-import java.math.BigDecimal;
-
-public interface CryptoQueryPort {
-     BigDecimal getIndicatorDataForCrypto(String currency, String indicator);
-}
