1ac19cac557b5a2560d858f470d9fb9d0942e12e
ergosumcogito <ergosumcogito@users.noreply.github.com>
2024-12-25 22:59:12 +0100

    Delete old entities, repositories, controllers and services

--- src/main/java/com/backendproject/cryptolytics/infrastructure/adapter/persistence/QueryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backendproject.cryptolytics.infrastructure.adapter.persistence;
-
-import com.backendproject.cryptolytics.domain.model.entities.Query;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QueryRepository extends JpaRepository<Query, Long> {
-}
