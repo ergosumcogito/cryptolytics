6ac6a94363d1fa0493da6f2760144a524eb0e501
ergosumcogito <ergosumcogito@users.noreply.github.com>
2025-01-06 23:34:26 +0100

    Add other indicators and their test data to db

--- scripts/init.sql
+++ scripts/init.sql
@@ -43,7 +43,10 @@ VALUES ('BTC', 'Bitcoin'),
 -- Insert test data into indicators
 INSERT INTO Indicator (name, description)
 VALUES ('Price', 'Current price of the currency'),
-       ('Volume', '24-hour trading volume');
+       ('Volume', '24-hour trading volume'),
+       ('Market_Cap', 'Market capitalization of the currency'),
+       ('Market_Cap_Rank', 'Rank by market capitalization'),
+       ('High_24h', 'Highest price in the last 24 hours');
 
 -- Insert test data into CurrencyIndicators (mapping currency and indicator)
 INSERT INTO Currency_Indicator (currency_id, indicator_id)
@@ -51,8 +54,16 @@ VALUES (1, 1), -- BTC - Price
        (2, 1), -- ETH - Price
        (1, 2), -- BTC - Volume
        (2, 2), -- ETH - Volume
-       (3, 1);
--- USDT - Price
+       (3, 1), -- USDT - Price
+       (1, 3), -- BTC - Market Cap
+       (2, 3), -- ETH - Market Cap
+       (3, 3), -- USDT - Market Cap
+       (1, 4), -- BTC - Market Cap Rank
+       (2, 4), -- ETH - Market Cap Rank
+       (3, 4), -- USDT - Market Cap Rank
+       (1, 5), -- BTC - High 24h
+       (2, 5), -- ETH - High 24h
+       (3, 5); -- USDT - High 24h
 
 -- Insert test data into CurrencyIndicatorValues
 INSERT INTO Currency_Indicator_Value (currency_indicator_id, value)
@@ -60,4 +71,13 @@ VALUES (1, 38500.75),  -- BTC - Price
        (2, 2800.45),   -- ETH - Price
        (3, 100000000), -- BTC - Volume
        (4, 1500000),   -- ETH - Volume
-       (5, 1); -- USDT - Price
+       (5, 1),         -- USDT - Price
+       (6, 720000000000), -- BTC - Market Cap
+       (7, 320000000000), -- ETH - Market Cap
+       (8, 83000000),     -- USDT - Market Cap
+       (9, 1),            -- BTC - Market Cap Rank
+       (10, 2),           -- ETH - Market Cap Rank
+       (11, 3),           -- USDT - Market Cap Rank
+       (12, 39000),       -- BTC - High 24h
+       (13, 3000),        -- ETH - High 24h
+       (14, 1.01);        -- USDT - High 24h
