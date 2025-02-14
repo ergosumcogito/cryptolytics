7307f77bfa1b60a7744c1a3fe2e952dc775703a2
ergosumcogito <ergosumcogito@users.noreply.github.com>
2025-02-14 13:03:14 +0100

    Add custom NotFoundException

---
+++ src/main/java/com/backendproject/cryptolytics/infrastructure/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.backendproject.cryptolytics.infrastructure.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
